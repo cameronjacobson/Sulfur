af13e902d0728b7bfba76a686cfc33fd121e9fdc
Cameron Jacobson <cameron@datashovel.com>
2012-12-28 03:13:59 -0500

    SulfurContext clear method

--- src/Sulfur/SulfurContext.php
+++ src/Sulfur/SulfurContext.php
@@ -4,7 +4,23 @@ namespace Sulfur;
 
 class SulfurContext
 {
+	public $host;
+	public $port;
+	public $mapping;
+	public $index;
+	public $request;
+	public $query;
+
 	public function __construct(){
 		
+	}
+
+	public function clear(){
+		$this->host = null;
+		$this->port = null;
+		$this->mapping = null;
+		$this->index = null;
+		$this->request = null;
+		$this->query = null;
 	}
 }
