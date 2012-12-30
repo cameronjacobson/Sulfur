0b7209ab7eb139966f812430af5b0222cad494ae
Cameron Jacobson <cameron@datashovel.com>
2012-12-30 14:12:23 -0500

    moved config directives under 'config' namespace

--- src/Sulfur/SulfurContext.php
+++ src/Sulfur/SulfurContext.php
@@ -4,22 +4,16 @@ namespace Sulfur;
 
 class SulfurContext
 {
-	public $host;
-	public $port;
-	public $mapping;
-	public $index;
 	public $request;
 	public $query;
+	public $config;
 
 	public function __construct(){
 		
 	}
 
 	public function clear(){
-		$this->host = null;
-		$this->port = null;
-		$this->mapping = null;
-		$this->index = null;
+		$this->config = null;
 		$this->request = null;
 		$this->query = null;
 	}
